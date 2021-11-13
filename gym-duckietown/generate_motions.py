fd12704de7f62c06aa2e03cacf50f7ad32e96dcc
gxshao <guoxin.shao@u.nus.edu>
2021-11-14 02:01:31 +0800

    fnished

--- gym-duckietown/generate_motions.py
+++ gym-duckietown/generate_motions.py
@@ -1,10 +1,13 @@
 import argparse
+from genericpath import getctime
+from math import nan
 from os import curdir, strerror
 from hybrid_planner import *
+from motion_planner import *
 import cv2
 import numpy as np
 from gym_duckietown.envs import DuckietownEnv
-from gym_duckietown.simulator import _update_pos
+from gym_duckietown.simulator import *
 import pyglet
 import time
 # declare the arguments
@@ -14,10 +17,10 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--max_steps', type=int, default=1500, help='max_steps')
 
 # You should set them to different map name and seed accordingly
-parser.add_argument('--map-name', '-m', default="map2_0", type=str)
+parser.add_argument('--map-name', '-m', default="map2_1", type=str)
 parser.add_argument('--seed', '-s', default=2, type=int)
-parser.add_argument('--start-tile', '-st', default="7,7", type=str, help="two numbers separated by a comma")
-parser.add_argument('--goal-tile', '-gt', default="1,1", type=str, help="two numbers separated by a comma")
+parser.add_argument('--start-tile', '-st', default="3,6", type=str, help="two numbers separated by a comma")
+parser.add_argument('--goal-tile', '-gt', default="7,1", type=str, help="two numbers separated by a comma")
 args = parser.parse_args()
 
 env = DuckietownEnv(
@@ -82,17 +85,275 @@ print("start tile:", start_pos, " goal tile:", goal)
 
 
 
-planner = HybridAStarPlanner(env)
-path, dots =planner.astar()
-for p in path:
-    for i in range(0, 30):
-        env.step([p[0][i], p[1][i]])
-        env.render()
-    print("current pose:",env.cur_pos)
-print(dots)
-print("done")
+planner = MotionPlanner(env)
+path =planner.astar()
+# path = [[7, 7], [6, 7], [5, 7], [5, 6], [5, 5], [4, 5], [3, 5], [3, 4], [3, 3], [2, 3], [1, 3], [1, 2], [1, 1]]
 
-# cv2.imshow("map", map_img)
-# cv2.waitKey(200)
+def closest_curve_point(pos, curves, angle=None):
+    curve_headings = curves[:, -1, :] - curves[:, 0, :]
+    #curve_headings = curve_headings / np.linalg.norm(curve_headings).reshape(1, -1)
+    
+    dir_vec = env.get_dir_vec(angle)
+    dot_prods = np.dot(curve_headings, dir_vec)
+
+    cps = curves[np.argmax(dot_prods)]
+    t = bezier_closest(cps, pos)
+    point = bezier_point(cps, t)
+    tangent = bezier_tangent(cps, t)
+    return point, tangent
+
+def get_lane_pos2(pos, angle, target_curve):
+    point, tangent = closest_curve_point(pos, target_curve, angle)
+    if point is None:
+        msg = 'Point not in lane: %s' % pos
+        raise Exception(msg)
+    assert point is not None
+    dirVec = env.get_dir_vec(angle)
+    dotDir = np.dot(dirVec, tangent)
+    dotDir = max(-1, min(1, dotDir))
+    posVec = pos - point
+    upVec = np.array([0, 1, 0])
+    rightVec = np.cross(tangent, upVec)
+    signedDist = np.dot(posVec, rightVec)
+    angle_rad = math.acos(dotDir)
+    if np.dot(dirVec, rightVec) < 0:
+        angle_rad *= -1
+    angle_deg = np.rad2deg(angle_rad)
+    return LanePosition(dist=signedDist, dot_dir=dotDir, angle_deg=angle_deg,
+                        angle_rad=angle_rad)
+# x1, x2, x3, x0
+# def bezier(ja, jb, start, end):
+#     p = []
+#     for s in range(10):
+#         if s %2 == 1:
+#             continue
+#         t = s / 10
+#         p.append([(1-t)*((1-t)*((1-t) * end[0] + t * ja[0]) + t * ((1-t) * ja[0] + t * jb[0])) + t*((1-t) * ((1-t) * ja[0]) + t * ((1-t) * jb[0] + t * start[0])), 0, \
+#         (1-t)*((1-t)*((1-t) * end[1] + t * ja[1]) + t*((1-t) * ja[1] + t * jb[1])) + t * ((1-t) * ((1-t) * ja[1] + t * jb[1]) + t * ((1-t) * jb[1] + t * start[1]))])
+#     return p
+
+# bezier_curve = bezier([0,1], [0, 1], [0, 0], [1, 1])
+# dots = np.array([], np.int32)
+# for p in bezier_curve:
+#     p[0] += 5.7
+#     p[2] += 6.3
+#    # dots = np.append(dots, [int((p[0]) * 100), int((p[2]) * 100)])
+
+# print(bezier_curve)
+
+def get_curve(curve, offset, map_img):
+    tile = env._get_tile(offset[0], offset[1])
+    print("angle match:", current_key, tile['angle'])
+    diff = current_key - tile['angle']
+    # if diff > 0:
+        #clockwise
+        # curve = np.rot90(curve, diff)
+    # elif diff < 0:
+        # anti
+        # curve = np.rot90(curve, 4 + diff)
+         
+    curve *= env.road_tile_size
+    mat = gen_rot_matrix(np.array([0, 1, 0]), (tile['angle']) * math.pi / 2)
+    curve = np.matmul(curve, mat)
+    curve += np.array([(offset[0] + 0.5) * env.road_tile_size, 0, (offset[1] + 0.5) * env.road_tile_size])
+
+    for c in curve:
+        dts = np.array([], np.int32)
+        for p in c:
+            dts = np.append(dts, [int((p[0]) * 100), int((p[2]) * 100)])
+        dts = dts.reshape((-1,1,2))
+        map_img = cv2.polylines(map_img,[dts],False,(0,0,255), thickness=3)
+        cv2.imshow("map", map_img)
+        cv2.waitKey(100)
+        
+        
+    return curve
+
+curve_list = {(1,1):np.array([
+                [
+                    [-0.30, 0, -0.20],
+                    [-0.30, 0, 0.20],
+                    [-0.20, 0, 0.30],
+                    [0.20, 0, 0.30],
+                ],
+            ]),
+            (-1,-1):np.array([
+                 [
+                    [-0.30, 0, -0.20],
+                    [0.20, 0, -0.20],
+                    [0.20, 0, -0.20],
+                    [0.20, 0, 0.30],
+                ],
+            ]),
+            # E-N
+            (1,-1): np.array([
+                [
+                    [-0.20, 0, -0.20],
+                    [0.20, 0, -0.20],
+                    [-0.20, 0, -0.20],
+                    [-0.20, 0, 0.20],
+                ]
+            ]),
+            # W-S
+            (-1,1): np.array([
+                [
+                     [ 0.2,  0.0  , 0.3],
+                     [ 0.2 , 0.0 , -0.2],
+                     [ 0.2 , 0.0 , -0.2],
+                     [0.3 , 0.0 , -0.2]
+                ]
+            ])
+            }
+# print(np.fliplr(curve_list[(1, -1)]))
+# print(np.flipud(curve_list[(1, -1)]))
+base_curve = np.array([
+                [
+                    [-0.20, 0, -0.20],
+                    [0.20, 0, -0.20],
+                    [-0.20, 0, -0.20],
+                    [-0.20, 0, 0.20],
+                ],])
+# np.array(
+#     [[[ 0.2 , 0.  , 0.2]
+#   [-0.2 , 0. ,  0.2]
+#   [-0.2 , 0. ,  0.2]
+#   [-0.2 , 0. , -0.2]]]
+# )
+
+# for i in range(1, 7):
+#     get_curve(base_curve, [i,7 - i], map_img)
+
+
+
+# pyglet.app.run()
+# p = [math.floor(env.cur_pos[0]), math.floor(env.cur_pos[2])]
+# print(path)
+# turn_curves = []
+# for i in range(1, len(path)):
+#     next_point = None
+#     if i + 1 < len(path):
+#         next_point = path[i + 1]
+#     if next_point is None:
+#         break
+#     if next_point[0] != p[0] and next_point[1] != p[1]:
+#         #print('Turning', next_point)
+#         local = np.array([p[0] - path[i][0], p[1] - path[i][1]], np.int32)
+#         target = np.array([next_point[0] - path[i][0], next_point[1] - path[i][1]], np.int32)
+#         key = tuple(local + target)
+#         print(key, local ,target)
+#         offset = [path[i][0] + 1, path[i][1]]
+#         turn_curves.append(get_curve(curve_list[key], offset, map_img))
+#         if len(turn_curves) == 2:
+#             break
+#     p = path[i]
+
+curve_angles = {(1, -1):1, (1, 1):0, (-1,1):3, (-1,-1):2}
+
+total_reward = 0
+dts = np.array([], np.int32)
+
+
+predicted_pos = start_pos
+turning_index = 0
+
+turning_curve = None
+local_target = None
+trig_target = None
+current_key = None
+
+pending_step = 0
+k_p = 10
+k_d = 3
+speed = 0.3
+
+def generate_action(robot_theta, target_theta):
+    if target_theta == -1:
+        return 0
+    theta = [{0: 0, 90: 1, 180: 1, 270: -1},
+             {0: -1, 90: 0, 180: 1, 270: 1},
+             {0: 1, 90: -1, 180: 0, 270: 1},
+             {0: 1, 90: 1, 180: -1, 270: 0}]
+
+    pose_set = {0: 0, 90: 1, 180: 2, 270: 3}
+    robot_theta = robot_theta * 90 % 360
+    target_theta = target_theta * 90 % 360
+    return theta[pose_set[robot_theta]][target_theta]
+
+target_theta = {(1,0):0,(0,1):3,(-1,0):2,(0,-1):1}
+
+hand_speed = 0
+hand_steering = 0
+while True:
+    time.sleep(0.01)
+    lane_pose = None
+    distance_to_road_center = 0
+    angle_from_straight_in_rads = 0
+
+    predicted_pos = [math.floor(env.cur_pos[0]), math.floor(env.cur_pos[2])]
+    speed = hand_speed
+    steering = hand_steering
+    if predicted_pos != trig_target:
+        lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
+        k_p = 80
+        k_d = 10
+        speed = 0.8
+        
+        distance_to_road_center = lane_pose.dist
+        angle_from_straight_in_rads = lane_pose.angle_rad
+        steering = k_p*distance_to_road_center + k_d*angle_from_straight_in_rads
+        
+    obs, reward, done, info = env.step([speed, steering])
+    total_reward += reward
+    
+    predicted_pos = [math.floor(env.cur_pos[0]), math.floor(env.cur_pos[2])]
+    # Turning finished
+    if predicted_pos != trig_target:
+        print("PID is controling")
+        local_target = None
+        trig_target = None
+        turning_curve = None
+        
+    # I Need To Turn
+    try:
+        path_index = path.index(predicted_pos)
+        if trig_target is None and len(path) > path_index + 2:
+            mid = path[path_index + 1]
+            next_point = path[path_index + 2]
+            if next_point[0] != predicted_pos[0] and next_point[1] != predicted_pos[1]:
+                target = np.array([next_point[0] - mid[0], next_point[1] - mid[1]], np.int32)
+                # print("I need", curve_angles[key])
+                trig_target = mid
+                print("I am  controling")
+                angle = int(abs(np.rad2deg(env.cur_angle)) % 360)
+                suffix = angle % 90
+                if suffix > 60:
+                    suffix = 1
+                else:
+                    suffix = 0
+                
+                angle = math.floor((angle / 90)) + suffix
+                t = target_theta[(next_point[0] - trig_target[0], next_point[1] - trig_target[1])]
+                c = generate_action(angle, t)
+                print(c,angle, t)
+                if c > 0:
+                    hand_speed = 0.4
+                    hand_steering = c * 0.6
+                else:
+                    hand_speed = 0.2
+                    hand_steering = c * 0.8
+    except:
+        pass
+    
+    d = [int(env.cur_pos[0] * 100), int(env.cur_pos[2] * 100)]
+    dts = np.append(dts,d)
+    dts = dts.reshape((-1,1,2))
+    map_img = cv2.polylines(map_img,[dts],False,(0,0,255), thickness=3)
+    cv2.imshow("map", map_img)
+    cv2.waitKey(10)
+    env.render()
+    if done:
+        break
+
+print("done")
 
 pyglet.app.run()
--- gym-duckietown/motion_planner.py
+++ gym-duckietown/motion_planner.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import cv2
 
 class Point:
     def __init__(self, x, y):
@@ -63,36 +64,30 @@ class MotionPlanner:
                     results.append(n_p)
         return results
     
-    def __init__(self, map_pixel, map_width, map_height, start, goal, robot_size=1) -> None:
-        self.map_data = map_pixel.tolist()
-        self.start = Point(start[0], start[1])
-        self.end = Point(goal[0], goal[1])
-        self.map_width = map_width
-        self.map_height = map_height
-        self.resolution = 100
-        
-        self.robot_size = robot_size
-        
-        # We cannot navigate to the resturant directly, so go to it's neighbour
-        if not self.is_drivable(self.end):
-            neighbours = self.get_neighbours(self.map_width, self.map_height, self.end)
-            for neighbour in neighbours:
-                if self.is_drivable(neighbour):
-                    self.end = neighbour
-        
-        print("start:", start)
-        print("goal :", goal)
+    def __init__(self, env):
+        self.env = env
+        self.map_img = None
+        self.map_width = 0
+        self.map_height = 0
     
-    def is_drivable(self, point):
+    def is_drivable(self, point)-> bool:
         obstacle = [0, 0, 0]
-        return obstacle != self.map_data[point.y * self.resolution][point.x * self.resolution]
-    
-    def astar(self):
+        mapdata = self.map_img.tolist()
+        return obstacle != mapdata[point.y * 100][point.x * 100]
 
-        open_list = [self.start]
+    def astar(self):
+        map_img, goal, start_pos = self.env.get_task_info()
+        start = Point(start_pos[0], start_pos[1])
+        start.angle = self.env.cur_angle
+        end = Point(goal[0], goal[1])
+        print(start.x, start.y, end.x, end.y)
+        self.map_img = map_img
+        self.map_width = self.map_img.shape[0]
+        self.map_height = self.map_img.shape[1]
+        open_list = [start]
         path = []
         close_set = set()
-
+        dts = np.array([], np.int32)
         while len(open_list) > 0:
             current_point = open_list[0]
             # priority was implemented manually...
@@ -104,28 +99,39 @@ class MotionPlanner:
             open_list.remove(current_point)
             close_set.add(current_point)
 
-            if current_point.equals(self.end):
+            if current_point.equals(end):
                 head = current_point
-                while head != self.start:
+                while head != start:
                     path.append([head.x, head.y])
+                    d = [int(head.x * 100), int(head.y * 100)]
+                    dts = np.append(dts, d)
                     head = head.prev
-                path.append([self.start.x, self.start.y])
+                path.append([start.x, start.y])
                 path.reverse()
                 break
 
-            neighbours = self.get_neighbours(self.map_width, self.map_height, current_point)
+            neighbours = self.get_neighbours(int(self.map_width / 100) , int(self.map_height / 100), current_point)
             for neighbour in neighbours:
                 ## Find v and w for neighbour 
                 if neighbour in close_set:
                     close_set.add(neighbour)
                     continue
-
                 if not self.is_drivable(neighbour):
                     continue
                 n_cost = current_point.g + self.cal_cost(current_point, neighbour)
                 if n_cost < neighbour.g or neighbour not in open_list:
                     neighbour.g = n_cost
-                    neighbour.h = self.cal_cost(neighbour, self.end)
+                    neighbour.h = self.cal_cost(neighbour, end)
                     neighbour.prev = current_point
+                    self.map_img = cv2.circle(self.map_img, (current_point.x * 100,current_point.y * 100), radius=0, color=(0, 0, 255), thickness=10)
+                    cv2.imshow("map", self.map_img)
+                    cv2.waitKey(10)
+                    print("current:",current_point.x, current_point.y, current_point.angle, "selected:", neighbour.x, neighbour.y, "Cost", neighbour.g)
                     open_list.append(neighbour)
+        
+        
+        dts = dts.reshape((-1,1,2))
+        self.map_img = cv2.polylines(self.map_img,[dts],False,(0,0,255), thickness=30)
+        cv2.imshow("map", self.map_img)
+        cv2.waitKey(10)
         return path
